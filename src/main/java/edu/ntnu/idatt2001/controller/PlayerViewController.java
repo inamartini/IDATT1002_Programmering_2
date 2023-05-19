10a5e95150b1626ad201bf8cee62a5b9a0e23e68
Ina <inamar@stud.ntnu.no>
2023-05-19 09:54:37 +0200

    Update playerView and controller to save player.

--- src/main/java/edu/ntnu/idatt2001/controller/PlayerViewController.java
+++ src/main/java/edu/ntnu/idatt2001/controller/PlayerViewController.java
@@ -1,14 +1,19 @@
 package edu.ntnu.idatt2001.controller;
 
+import edu.ntnu.idatt2001.base.Player;
 import javafx.scene.image.Image;
 
 public class PlayerViewController {
 
   private static PlayerViewController playerViewController = new PlayerViewController();
   private Image playerImage;
+  private Player player;
+  private String playerName;
+  private int playerHealth;
+  private int playerGold;
+
 
   private PlayerViewController() {
-
   }
 
   public static PlayerViewController getInstance() {
@@ -21,6 +26,22 @@ public class PlayerViewController {
 
   public void setPlayerImage(Image playerImage) {
     this.playerImage = playerImage;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
+
+  public Player createPlayer(String playerName,int playerHealth, int playerGold) {
+     this.player = new Player.Builder(playerName)
+        .health(playerHealth)
+        .gold(playerGold)
+        .build();
+     return player;
   }
 
 }
--- src/main/java/edu/ntnu/idatt2001/view/PlayerView.java
+++ src/main/java/edu/ntnu/idatt2001/view/PlayerView.java
@@ -1,9 +1,9 @@
 package edu.ntnu.idatt2001.view;
 
 import edu.ntnu.idatt2001.controller.PlayerViewController;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -14,17 +14,18 @@ public class PlayerView extends View {
 
   private ScreenController screenController;
   private StackPane root;
+  private BorderPane borderPane;
   private ImageView playerImageView;
-  private PlayerViewController playerViewController;
+  private PlayerViewController playerViewController = PlayerViewController.getInstance();
 
   public PlayerView(ScreenController screenController) {
-    this.screenController = screenController;
-    this.playerViewController = PlayerViewController.getInstance();
     this.root = new StackPane();
-    this.setUp();
+    this.borderPane = new BorderPane();
+    borderPane.setCenter(root);
+    this.screenController = screenController;
   }
 
-  public StackPane getPane() {
+  public Pane getPane() {
     return this.root;
   }
 
@@ -77,27 +78,107 @@ public class PlayerView extends View {
     HBox nameBox = new HBox(10, playerName, nameField);
     nameBox.setAlignment(Pos.CENTER);
 
-    Text playerHealth = new Text("Health:");
-    TextField healthField = new TextField();
-    healthField.setPrefWidth(200);
+    Text health = new Text("Health: ");
+    health.getStyleClass().add("playerView-slider-text");
+    Text playerHealth = new Text("0");
 
-    HBox healthBox = new HBox(10, playerHealth, healthField);
+    Slider healthSlider = new Slider();
+    healthSlider.setMin(0);
+    healthSlider.setMax(10);
+    healthSlider.setShowTickLabels(true);
+    healthSlider.setShowTickMarks(true);
+    healthSlider.setMajorTickUnit(5);
+    healthSlider.setBlockIncrement(1);
+    healthSlider.setPrefWidth(150);
+    healthSlider.setValue(0);
+
+    healthSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+      healthSlider.setValue(newValue.intValue());
+      String valueAsString = Integer.toString(newValue.intValue());
+      playerHealth.setText(valueAsString);
+    });
+    healthSlider.getStyleClass().add("playerView-slider");
+
+    //playerHealth.textProperty().bind(healthSlider.valueProperty().asString());
+    playerHealth.getStyleClass().add("playerView-slider-text");
+
+    //TextField healthField = new TextField();
+    //healthField.setPrefWidth(200);
+
+
+    HBox healthBox = new HBox(10, health, healthSlider, playerHealth);
     healthBox.setAlignment(Pos.CENTER);
 
-    Text playerGold = new Text("Gold:   ");
-    TextField goldField = new TextField();
-    goldField.setPrefWidth(200);
+    Text gold = new Text("Gold:");
+    gold.getStyleClass().add("playerView-slider-text");
+    Text playerGold = new Text("0");
 
-    HBox goldBox = new HBox(10, playerGold, goldField);
+    Slider goldSlider = new Slider();
+    goldSlider.setMin(0);
+    goldSlider.setMax(200);
+    goldSlider.setShowTickLabels(true);
+    goldSlider.setShowTickMarks(true);
+    goldSlider.setMajorTickUnit(50);
+    goldSlider.setMinorTickCount(25);
+    goldSlider.setBlockIncrement(5);
+    goldSlider.setPrefWidth(150);
+    goldSlider.setValue(0);
+
+    goldSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+      goldSlider.setValue(newValue.intValue());
+      String valueAsString = Integer.toString(newValue.intValue());
+      playerGold.setText(valueAsString);
+    });
+    goldSlider.getStyleClass().add("playerView-slider");
+
+    //playerGold.textProperty().bind(goldSlider.valueProperty().asString());
+    playerGold.getStyleClass().add("playerView-slider-text");
+
+
+    //TextField goldField = new TextField();
+    //goldField.setPrefWidth(200);
+
+    HBox goldBox = new HBox(10, gold, goldSlider, playerGold);
     goldBox.setAlignment(Pos.CENTER);
 
     VBox playerDetails = new VBox(10, nameBox, healthBox, goldBox);
     playerDetails.setAlignment(Pos.CENTER);
 
-    Button btnCreatePlayer = new Button("Create Player");
+    Button btnCreatePlayer = new Button("Create player");
     btnCreatePlayer.setOnAction(e -> {
-      playerViewController.setPlayerImage(playerImageView.getImage());
-      screenController.activate("gameView");
+      if (nameField.getText().isEmpty()) {
+        createAlert("Invalid input", "Name field is empty",
+                "Please enter a name to create a player");
+      } else {
+        /*try {
+          //int health = Integer.parseInt(healthField.getText());
+          int gold = Integer.parseInt(goldField.getText());
+
+          if (healthField.getText().isEmpty()) {
+            createAlert("Invalid input", "Health field is empty",
+                    "Please enter a number for health");
+          } else if (goldField.getText().isEmpty()) {
+            createAlert("Invalid input", "Gold field is empty",
+                    "Please enter a number for gold");
+          } else if (health <0) {
+            createAlert("Invalid input", "Health is a negative number",
+                    "Please enter a positive number for health");
+          } else if (gold < 0) {
+            createAlert("Invalid input", "Gold is a negative number",
+                    "Please enter a positive number for gold");
+          } else {
+            playerViewController.createPlayer(nameField.getText(), health, gold);
+            screenController.activate("gameView");
+            playerViewController.setPlayerImage(playerImageView.getImage());
+          }
+        } catch (NumberFormatException exception) {
+          createAlert("Invalid input", "Health or gold is not a number",
+                  "Please enter a number for health and gold");
+        }*/
+        playerViewController.setPlayerImage(playerImageView.getImage());
+        playerViewController.createPlayer(nameField.getText(), (int) healthSlider.getValue(), (int) goldSlider.getValue());
+        screenController.activate("gameView");
+      }
     });
     btnCreatePlayer.getStyleClass().add("playerView-create-button");
 
@@ -111,12 +192,19 @@ public class PlayerView extends View {
     VBox content = new VBox(10, title, characterBox, playerDetails, createPlayerBox);
     content.setAlignment(Pos.CENTER);
 
-    BorderPane borderPane = new BorderPane();
     borderPane.setTop(btnGoBack);
     borderPane.setCenter(content);
 
     root.getChildren().addAll(borderPane);
     root.setAlignment(Pos.CENTER);
+  }
+
+  public void createAlert(String title, String headerText, String contentText) {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setTitle(title);
+    alert.setHeaderText(headerText);
+    alert.setContentText(contentText);
+    alert.showAndWait();
   }
 
   protected void resetPane() {
