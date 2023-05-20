519186f0cab00f22be4c279e4405f4bbd7f18e07
Ina <inamar@stud.ntnu.no>
2023-05-20 15:52:00 +0200

    Change accessibility in View.

--- src/main/java/edu/ntnu/idatt2001/view/View.java
+++ src/main/java/edu/ntnu/idatt2001/view/View.java
@@ -2,12 +2,12 @@ package edu.ntnu.idatt2001.view;
 
 import javafx.scene.layout.Pane;
 
-abstract class View {
+public abstract class View {
 
-  protected abstract void setUp();
+  public abstract void setUp();
 
   abstract void resetPane();
 
-  abstract Pane getPane();
+  public abstract Pane getPane();
 
 }
